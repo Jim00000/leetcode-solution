3b2210022b0c6244c88a4bfe27f9aaba4fbe3296
Jim00000 <good0121good@gmail.com>
2019-11-25 18:20:18 +0800

    Add testcases for 'Print in order' quiz

--- Concurrency/Print in Order/test/test.cpp
+++ Concurrency/Print in Order/test/test.cpp
@@ -1,3 +1,27 @@
+#include "VMain.hpp"
 #include <gtest/gtest.h>
+#include <type_traits>
 
-TEST(test_add, add_1_plus_1) { EXPECT_EQ(2, 1 + 1); }
+template <typename STRING, typename = std::enable_if_t<
+                               std::is_convertible_v<STRING, std::string>>>
+void CheckOutput(STRING &&Output) {
+  const std::string ExpectedOutput = "firstsecondthird";
+  EXPECT_EQ(Output, ExpectedOutput);
+}
+
+template <int A, int B, int C> void TestTemplate() {
+  static_assert(1 <= A && A <= 3);
+  static_assert(1 <= B && B <= 3);
+  static_assert(1 <= C && C <= 3);
+  testing::internal::CaptureStdout();
+  VMain(A, B, C);
+  std::string Output = testing::internal::GetCapturedStdout();
+  CheckOutput(Output);
+}
+
+TEST(test_print_in_order, order_1_2_3) { TestTemplate<1, 2, 3>(); }
+TEST(test_print_in_order, order_1_3_2) { TestTemplate<1, 3, 2>(); }
+TEST(test_print_in_order, order_2_1_3) { TestTemplate<2, 1, 3>(); }
+TEST(test_print_in_order, order_2_3_1) { TestTemplate<2, 3, 1>(); }
+TEST(test_print_in_order, order_3_2_1) { TestTemplate<3, 2, 1>(); }
+TEST(test_print_in_order, order_3_1_2) { TestTemplate<3, 1, 2>(); }
