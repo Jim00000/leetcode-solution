e6e5ff351ba285659a2b5f9c19886d144d80c3cb
Jim00000 <good0121good@gmail.com>
2020-01-02 17:58:50 +0800

    temporary dev 1

--- Algorithms/Container With Most Water/src/Solution.cpp
+++ Algorithms/Container With Most Water/src/Solution.cpp
@@ -1,5 +1,21 @@
+#include <iostream>
+#include <queue>
+
 #include "Solution.hpp"
 
 using namespace leetcode;
 
-int Solution::maxArea(std::vector<int>& height) { return 0; }
+static int maxmin(std::vector<int>& v)
+{
+    std::priority_queue<int> maxheap{v.cbegin(), v.cend()};
+    const unsigned int _1st_large_val = maxheap.top();
+    maxheap.pop();
+    const unsigned int _2st_large_val = maxheap.top();
+}
+
+int Solution::maxArea(std::vector<int>& height)
+{
+    // WIP
+    maxmin(height);
+    return 0;
+}
